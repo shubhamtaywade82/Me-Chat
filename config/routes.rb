8b0cad771a0d1d334413529356b757f543010586
shubhamtaywade82 <shubhamtaywade82@gmail.com>
2020-06-04 10:44:56 +0530

    Added navigation menu, enable drop down, nav partial

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   root 'chatroom#index'
+  get 'login', to: 'home#login'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
