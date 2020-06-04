6621ccd8f3d7a92bee0192e7ea2d0e6cb7e12472
shubhamtaywade82 <shubhamtaywade82@gmail.com>
2020-06-04 19:16:40 +0530

    add messages

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,13 +16,8 @@ require("semantic-ui-sass");
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 $(document).on("turbolinks:load", function () {
-    $(".ui.dropdown").dropdown();
-    $('.message .close')
-        .on('click', function () {
-            $(this)
-                .closest('.message')
-                .transition('fade')
-                ;
-        })
-        ;
+  $(".ui.dropdown").dropdown();
+  $(".message .close").on("click", function () {
+    $(this).closest(".message").transition("fade");
+  });
 });
