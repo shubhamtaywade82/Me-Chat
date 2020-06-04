4ea3cbeb3b8b4c2c984c3ee11af5aa1c646d196d
shubhamtaywade82 <shubhamtaywade82@gmail.com>
2020-06-04 18:38:37 +0530

    add authentication system to the app

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 class ChatroomController < ApplicationController
+  before_action :require_user
   def index
     @messages = Message.all
+    # @messages = Message.custom_display
   end
 end
