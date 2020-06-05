b1a08f73faf73d72ec164d300c630cbf67ec7097
shubhamtaywade82 <shubhamtaywade82@gmail.com>
2020-06-05 20:47:05 +0530

    modify and broadcast message, display using partial

--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -1,8 +1,9 @@
-import consumer from "./consumer"
+import consumer from "./consumer";
 
 consumer.subscriptions.create("ChatroomChannel", {
   connected() {
     // Called when the subscription is ready for use on the server
+    console.log("Connected to the room!");
   },
 
   disconnected() {
@@ -11,5 +12,6 @@ consumer.subscriptions.create("ChatroomChannel", {
 
   received(data) {
     // Called when there's incoming data on the websocket for this channel
-  }
+    $("#message-container").append(data.content);
+  },
 });
